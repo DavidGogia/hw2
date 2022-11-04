e66791f4c11856ec874cec5e9b566ad44f03febb
DavidGogia <117478580+DavidGogia@users.noreply.github.com>
2022-11-04 23:06:38 +0400

    Add files via upload

---
+++ src.sv
@@ -0,0 +1,18 @@
+module src(
+	input [31:0] PC_out,
+	input [1:0] select,
+	input [31:0] Rs,
+	input [31:0] Rn,
+	output reg [31:0] out
+);
+
+always_comb begin
+	case(select)
+		0: 	out = Rn;
+		1: 	out = Rs;
+		2:	out = PC_out;
+		default: out = 0;
+	endcase
+end	
+
+endmodule
